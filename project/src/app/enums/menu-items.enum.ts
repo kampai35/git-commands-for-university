752942ad0f3b3c8e44a2f27e2a2a31da362d2794
Damian <damianpolit@interia.pl>
2023-01-13 15:02:25 +0100

    added routing, commands page

---
+++ project/src/app/enums/menu-items.enum.ts
@@ -0,0 +1,4 @@
+export enum MenuItems {
+  CommandsPage = 'commands-page',
+  HomePage = ''
+}
---
+++ project/src/components/commands-page/commands-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commands-page',
+  templateUrl: './commands-page.component.html',
+  styleUrls: ['./commands-page.component.scss']
+})
+export class CommandsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
